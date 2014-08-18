3176dd53cb30cde2b2289df7c2cd2aaac2328cc6
Asif Ahmed <asifahmed2011@gmail.com>
2014-08-17 18:03:24 -0700

    waitstaff calc

---
+++ thinkful-angular/1.3.7/app.js
@@ -0,0 +1,68 @@
+var myApp = angular.module('waiterApp', []);
+
+myApp.controller('WaiterCtrl', ['$scope', function($scope){
+
+	$scope.resetDetails = function(){
+		$scope.mealPrice = '';
+		$scope.taxRate = '';
+		$scope.tipPercentage = '';
+	};
+
+	$scope.initCharges = function(){
+		$scope.subtotal = 0;
+		$scope.tip = 0;
+		$scope.total = 0;
+	};
+
+	$scope.initEarnings = function(){
+		$scope.tipTotal = 0;
+		$scope.mealCount = 0;
+	};
+
+	$scope.init = function(){
+		console.log("HEYY");
+		$scope.formError = "";
+		$scope.resetDetails();
+		$scope.initCharges();
+		$scope.initEarnings();
+	};
+
+	$scope.init();
+
+	$scope.submitDetails = function(){
+		if($scope.enterPriceForm.$invalid){
+			$scope.formError = "Please enter valid values ";
+		}
+		else{
+			$scope.formError = "";
+			$scope.tipTotal += $scope.tip;
+			$scope.mealCount++;
+		}
+	};
+
+	$scope.$watchGroup(['mealPrice', 'taxRate', 'tipPercentage'], function(newValues, oldValues, scope) {
+		if($scope.enterPriceForm.$invalid){
+			$scope.initCharges();
+		}
+		else{
+			$scope.formError = "";
+			$scope.subtotal = $scope.mealPrice * (1 + $scope.taxRate/100);
+			$scope.tip = $scope.mealPrice * ($scope.tipPercentage/100);
+			$scope.total = $scope.subtotal + $scope.tip;
+		}
+	});
+
+	$scope.$watchGroup(['tipTotal', 'mealCount'], function(newValues, oldValues, scope) {
+		if($scope.mealCount != 0){
+			$scope.avgTipPerMeal = $scope.tipTotal/$scope.mealCount;
+		}
+		else{
+			$scope.avgTipPerMeal = 0;
+		}
+		
+	});
+
+
+
+
+}]);
